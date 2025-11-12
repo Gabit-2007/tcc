0810d4097915bce197df496612d83a5153608026
Gabit-2007 <gabilovemel@gmail.com>
2025-11-11 23:24:02 -0300

    feat: Update styles across multiple pages for improved responsiveness and layout consistency

--- frontend/src/pages/detalhes/index.jsx
+++ frontend/src/pages/detalhes/index.jsx
@@ -1,5 +1,7 @@
 import { React, useEffect, useState } from "react";
 import { useLocation } from "react-router";
+import "./styles.scss";
+import Cabecalho from "../../components/cabecalho";
 
 export default function DetalhesPage(){
     const [item, setItem] = useState({});
@@ -11,17 +13,24 @@ export default function DetalhesPage(){
 
     return(
         <div>
+            <Cabecalho></Cabecalho>
+        <div className="tudo">
+            <div className="imagem"> 
             <img src={item.imagem} />
-            <div>
+            </div>
+            <div className="info">
                 <h1>{item.nome}</h1>
+                <div className="peee">
                 <p>Localização: {item.localizacao}</p>
                 <p>Valor: {item.preco}</p>
                 <p>Avaliação: {item.avaliacao}/5</p>
                 <p>Horário: {item.horario}</p>
                 <p>Classificação: {item.classificacao}</p>
+                </div>
 
                 <a href={item.rota}><button>Visitar!</button></a>
             </div>
+        </div>
         </div>
     );
 }
